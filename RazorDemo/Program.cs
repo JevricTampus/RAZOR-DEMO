16b8855222f5b6a4232263b924f13a63f3826c38
JevricTampus <jevric.tampus04@gmail.com>
2025-09-08 02:30:29 +0000

    razor.html

---
+++ RazorDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html >
+< html >
+    < head >
+        < title > RazorDemo </ title >
+</ head >
+< body >
+         < h1 > Lisa </ h1 >
+         < h2 > Rose </ h2 >a
+         < H3 > Jennie </ H3 >
+         < h4 > Jisoo </ h4 >
+         <h5  > BlackPink</h5>
+         <a href="https://tse4.mm.bing.net/th/id/OIP.Gy7eKuMP0GV8DdVPGEWTwwAAAA?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3"
+         jpg"><h6>BlackPink</h6></a>
+         <p> Ang pamagat na ito ay nagpapakita ng larawan ng isang grupo.</p>
+         <img rc-https://blogger.googleusercontent.com/img/b/R29vZZxl/ AVvXsEjGmiqAvm8wktWsFven_hcQvfTdyYVk_ZH_GRTh-aTh6MfbLhd4GNcTi8gKkz2UM7NTol s_d150MgQR28aWipipUYQKAOv5ht74jo8G7Cvwevhk _KVtMXRIj-290k6LCqWw5wOGyUwheCXo/ $1600/ee. ipg">
+    </ body >
+</html>
